9d0805b1b7aafc523c728bf9da16f07b8509bc14
晓旭 唐 <1165907265@qq.com>
2020-08-21 16:35:46 +0800

    C#学习新增

--- Csharp_if/Csharp_if/Form1.cs
+++ Csharp_if/Csharp_if/Form1.cs
@@ -19,15 +19,84 @@ namespace Csharp_if
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //int i = 0;
+            //if (i==0)
+            //{
+            //    MessageBox.Show("hello,world!");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("false11");
+            //}
+            //int a = 4;
+            //switch (a)
+            //{
+            //    case 1:
+            //        MessageBox.Show("这是1");
+            //        break;
+            //    case 2:
+            //        MessageBox.Show("这是2");
+            //        break;
+            //    case 3:
+            //        MessageBox.Show("这是3");
+            //        break;
+            //    default:
+            //        MessageBox.Show("你给出的东西不在条件内");
+            //        break;
+            //}
+
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    MessageBox.Show("这是第" + (i + 1) + "循环，结果是" + i);
+            //}
+            //int i = 0;
+            //while (i<5)
+            //{
+            //    MessageBox.Show("这是第" + (i + 1) + "循环，结果是" + i);
+            //    i++;
+            //}
+            //do
+            //{
+            //    MessageBox.Show("这是第" + (i + 1) + "循环，结果是" + i);
+            //    i++;
+            //} while (i<5);
+
+            //decimal a = 1.9m;
+            //decimal b = 16m;
+            //decimal totalless;
+            //decimal total;
+            //int d = (int)(a * b / 10);
+            //decimal es = 0m;
+            //for (int i = 0; i < d; i++)
+            //{
+            //    es += Decimal.Parse((10 * 0.75).ToString());
+            //}
+            //totalless = ( a * b % 10);
+            //total = es + totalless;
+            //MessageBox.Show(total.ToString());
+
+            /*Console.WriteLine( Convert.ToInt32(null));*/
+           /* string[] schools = {"湖大","中南", "师范", "国防", "湘大", "科大" };
             int i = 0;
-            if (i==0)
+            string ss = "";
+            bool flag = true;
+            while (ss != "湘大")
             {
-                MessageBox.Show("hello,world!");
+                if (i >= schools.Length)
+                {
+                    MessageBox.Show("没有该数据！");
+                    flag = false;
+                    break;
+                }
+                ss = schools[i];
+                i++;
             }
-            else
+            if (flag)
             {
-                MessageBox.Show("false11");
-            }
+                MessageBox.Show("已经找到了" + ss);
+            }*/
+
         }
     }
 }
--- Csharp_if/Csharp_if/Program.cs
+++ Csharp_if/Csharp_if/Program.cs
@@ -17,6 +17,7 @@ namespace Csharp_if
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
